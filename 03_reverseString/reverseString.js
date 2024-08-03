6d37d444e5250a1ced0301065b2bfd02099ebdc8
Aria Ho <ariaho99@proton.me>
2024-08-03 18:04:36 +0800

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const inputArray = string.split("");
+  const revArray = [];
+  for (let i = inputArray.length - 1; i >= 0; i--) {
+    revArray.push(inputArray.at(i));
+  }
+  return revArray.join("");
 };
 
 // Do not edit below this line
