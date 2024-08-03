870fd8020fb62dfcb98285e5d46fd44949e744b4
Aria Ho <ariaho99@proton.me>
2024-08-03 18:18:28 +0800

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...targets) {
+  const newArray = [];
+  for (const elem of array) {
+    let isTarget = false;
+    for (const target of targets) {
+      if (elem === target) {
+        isTarget = true;
+        break;
+      }
+    }
+
+    if (!isTarget) {
+      newArray.push(elem);
+    }
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
