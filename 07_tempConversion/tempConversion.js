676ef33bc679736ad41819d720f59d135b2c8082
Aria Ho <ariaho99@proton.me>
2024-08-03 18:40:34 +0800

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp = (temp - 32) / 1.8;
+  return Math.round(convertedTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let convertedTemp = temp * 1.8 + 32;
+  return Math.round(convertedTemp * 10) / 10;
 };
 
 // Do not edit below this line
